d4c08ddcca583f887fabd31b37e779989c4ee74f
Nathan <38576090+TheTrueCoder@users.noreply.github.com>
2021-08-27 22:38:13 +1200

    Add libs
    Fix gitignore

---
+++ quotepage/src/lib/Quote.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let text = ""
+</script>
+
+<div>
+    <p>{text}</p>
+</div>
+
+<style>
+    div {
+        width: 100%;
+        background-color: rgb(100, 100, 100);
+        border-radius: 10px;
+    }
+
+    p {
+        padding: 15px;
+        color: white;
+        font-size: 20px;
+    }
+</style>
